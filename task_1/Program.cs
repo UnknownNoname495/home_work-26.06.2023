9c5e14c91dfe18c2d81c72c52eb62419a87f1479
AlexUnknown <saint.kaizer@yandex.ru>
2023-06-30 21:04:19 +0300

    .\task_1_modified\

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -6,5 +6,12 @@
 
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100 || number > 1000)
+{
+    Console.Write("Введите трёхзначное число.");
+    return;
+}
+
 int result = (number / 10) % 10;
 Console.WriteLine($"В числе {number} вторая цифра -> {result}");
